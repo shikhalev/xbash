ab0ee8c16fa7768b576a2efc35c0a3a63cc973b7
Ivan Shikhalev <shikhalev@gmail.com>
2021-09-02 23:19:41 +0500

    command_not_found_handle() implementation

--- xbash.bash
+++ xbash.bash
@@ -1,3 +1,9 @@
+# Есть опасность, что после переопределений разных данный файл
+# будет загружен сам из себя, что приведет к бесконечной рекурсии.
+if [ -z ${xb_flag+anything} ]; then
+  xb_flag=loaded;
+# Можно было бы проверять реально используемую переменную...
+# Но это чревато при дальнейших изменениях.
 
 # Предустановленные сообщения.
 xb_command_not_found_msg='Команда не найдена: %s\n';
@@ -11,16 +17,27 @@ xb_system_dir=/etc/xbash.d;
 xb_user_dir=$HOME/.local/xbash;
 xb_project_file=./.xbash.bash;
 xb_system_config=/etc/xbash;
-xb_user_config=$HOME/.local/xbash;
+xb_user_config=$HOME/.config/xbash;
 
+# Инициализация основных переменных.
+# unset нужен для возможности повторной загрузки данного файла.
+
+unset xb_disable;
 declare -a xb_disable;
+unset xb_disable_prompt;
 declare -a xb_disable_prompt;
+unset xb_disable_commands;
 declare -a xb_disable_commands;
+unset xb_disable_completion;
 declare -a xb_disable_completion;
 
+unset xb_checks;
 declare -A xb_checks;
+unset xb_prompts;
 declare -A xb_prompts;
+unset xb_commands;
 declare -A xb_commands;
+unset xb_completions;
 declare -A xb_completions;
 
 # Считываем заданные конфигурацией установки:
@@ -64,8 +81,45 @@ if [ ! -z "$xb_user" ]; then
   done;
 fi;
 
+# Подготавливаем ассоциативный массив для быстрого выяснения disable.
+unset xb_disabled_commands;
+declare -a xb_disabled_commands;
+xb_disabled_commands+=( ${xb_disable[*]} );
+xb_disabled_commands+=( ${xb_disable_commands[*]});
+unset xb_disabled_commands_hash;
+declare -A xb_disabled_commands_hash;
+for key in ${xb_disabled_commands[*]}; do
+  xb_disabled_commands_hash[$key]='true';
+done;
+
 # Подставляем субкоманды.
 command_not_found_handle() {
+  unset xb_subcommands;
+  declare -g -A xb_subcommands;
+  for key in ${!xb_commands[*]}; do
+    echo ${xb_disabled_commands_hash[$key]};
+    if [ ${xb_disabled_commands_hash[$key]} = 'true' ]; then
+      continue;
+    fi;
+    if ${xb_checks[$key]}; then
+      ${xb_commands[$key]};
+    fi;
+    # По итогу мы должны получить таблицу субкоманд актуальных и не запрещенных.
+  done;
+  local subcmd=$1;
+  if [ -n "${xb_subcommands[$subcmd]}" ]; then
+    shift;
+    ${xb_subcommands[$subcmd]} $@;
+    return $?;
+  fi;
   printf "$xb_command_not_found_msg" $1;
   return $xb_command_not_found_num;
 }
+
+echo ${BASH_SOURCE[*]};
+
+# См. верх файла.
+# Сбрасываем флаг мы тут затем, чтобы разрешить повторную загрузку данного файла,
+# оставив запрет только на рекурсивную.
+  unset xb_flag;
+fi;
